6f489d5a34e63677549ad8a9e7930f70f8442830
metinnalci@outlook.com <metinnalci@outlook.com>
2021-02-16 14:30:59 +0300

    Kişi detay operasyonu yapıldı.

--- PhoneBookAPI/Controllers/UserController.cs
+++ PhoneBookAPI/Controllers/UserController.cs
@@ -44,6 +44,39 @@ namespace PhoneBookAPI.Controllers
             return users;
         }
 
+        [Route("user/{id}")]
+        [HttpGet]
+        public IActionResult GetUserDetail(int id)
+        {
+            User user = _phoneBookContext.Users.Find(id);
+            if (user != null)
+            {
+                User userdetail = _phoneBookContext.Users.Include(q => q.ContactTypes).Select(t => new User
+                {
+                    ID = t.ID,
+                    Name = t.Name,
+                    SurName = t.SurName,
+                    Company = t.Company,
+                    Deleted = t.Deleted,
+                    ContactTypes = (List<ContactTypes>)t.ContactTypes.Select(p => new ContactTypes
+                    {
+                        ID = p.ID,
+                        UserID = p.UserID,
+                        Phone = p.Phone,
+                        Email = p.Email,
+                        Location = p.Location
+                    })
+                }).Where(q => q.Deleted == false).FirstOrDefault(ı => ı.ID == id);
+
+                return Ok(userdetail);
+
+            }
+            else
+            {
+                return BadRequest($"ID numarası {id} olan kişi bulunamadı!");
+            }
+        }
+
 
     }
 }
